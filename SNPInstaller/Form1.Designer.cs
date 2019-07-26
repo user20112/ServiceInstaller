4e64148389408b341f718bac56d5361e98052fe6
user20112 <devlinpaddock@gmail.com>
2019-07-26 12:45:08 -0400

    Initial Commit
    Installs the SNP Service currently but it could be adapted fo any service. must be run as admin.

---
+++ SNPInstaller/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SNPInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.UninstallButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(22, 2);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(75, 23);
+            this.InstallButton.TabIndex = 0;
+            this.InstallButton.Text = "Instal";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // UninstallButton
+            // 
+            this.UninstallButton.Location = new System.Drawing.Point(22, 31);
+            this.UninstallButton.Name = "UninstallButton";
+            this.UninstallButton.Size = new System.Drawing.Size(75, 23);
+            this.UninstallButton.TabIndex = 1;
+            this.UninstallButton.Text = "uninstall";
+            this.UninstallButton.UseVisualStyleBackColor = true;
+            this.UninstallButton.Click += new System.EventHandler(this.UninstallButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 66);
+            this.Controls.Add(this.UninstallButton);
+            this.Controls.Add(this.InstallButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button UninstallButton;
+    }
+}
+
---
+++ SNPInstaller/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SNPInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            bool isElevated;
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            if (!isElevated)
+                MessageBox.Show("Please Launch Me in Administrative mode! ( rightclick and hot run as admin i need this to install the service)");
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            string strCmdText = "/K "+ Directory.GetCurrentDirectory()+"\\InstallUtil.exe "+ Directory.GetCurrentDirectory()+"\\SNP\\SNPService.exe";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        private void UninstallButton_Click(object sender, EventArgs e)
+        {
+            string strCmdText = "/K " + Directory.GetCurrentDirectory() + "\\InstallUtil.exe /u " + Directory.GetCurrentDirectory() + "\\SNP\\SNPService.exe";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
