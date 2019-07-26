2adf179f3a4ac5dd8c3ad358760ee0cfe44085ca
user20112 <devlinpaddock@gmail.com>
2019-07-26 15:44:09 -0400

    Changed it to allow service name to change through app.config

--- SNPInstaller/Form1.cs
+++ SNPInstaller/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.IO;
@@ -29,15 +30,16 @@ namespace SNPInstaller
                 MessageBox.Show("Please Launch Me in Administrative mode! ( rightclick and hot run as admin i need this to install the service)");
         }
 
+
         private void InstallButton_Click(object sender, EventArgs e)
         {
-            string strCmdText = "/K "+ Directory.GetCurrentDirectory()+"\\InstallUtil.exe "+ Directory.GetCurrentDirectory()+"\\SNP\\SNPService.exe";
+            string strCmdText = "/K " + Directory.GetCurrentDirectory() + "\\InstallUtil.exe " + Directory.GetCurrentDirectory() + "\\Service\\" + ConfigurationManager.AppSettings["EXEname"];//change /K to /C to make the cmd prompt close after install or uninstall
             System.Diagnostics.Process.Start("CMD.exe", strCmdText);
         }
 
         private void UninstallButton_Click(object sender, EventArgs e)
         {
-            string strCmdText = "/K " + Directory.GetCurrentDirectory() + "\\InstallUtil.exe /u " + Directory.GetCurrentDirectory() + "\\SNP\\SNPService.exe";
+            string strCmdText = "/K " + Directory.GetCurrentDirectory() + "\\InstallUtil.exe /u " + Directory.GetCurrentDirectory() + "\\Service\\" + ConfigurationManager.AppSettings["EXEname"];
             System.Diagnostics.Process.Start("CMD.exe", strCmdText);
         }
     }
